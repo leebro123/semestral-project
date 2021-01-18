a256acd681a620fad6f7cf92729a626de15ef1dc
Libor Michálek <libor.michlek@gmail.cz>
2021-01-18 09:25:13 +0100

    [smartplugfront] Add date pickers for selecting from & to datetime

--- SmartPlug/smartplugfront/src/components/GeneralLineChartLayout.js
+++ SmartPlug/smartplugfront/src/components/GeneralLineChartLayout.js
@@ -4,30 +4,41 @@ import axios from 'axios';
 import {useEffect, useState} from 'react'
 import links from '../links';
 import GeneralLineChart from './GeneralLineChart';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const GeneralLineChartLayout = (props) => {
-    const [emeters, setEmeters] = useState(null);
+    const [emeters, setEmeters] = useState([]);
     const [isEmeterLoading, setIsEmeterLoading] = useState(false);
+    const [fromDate, setFromDate] = useState(null);
+    const [toDate, setToDate] = useState(null);
 
     useEffect(() => {
         setIsEmeterLoading(true);
-        // axios.get(links.all).then((response) => {
-        axios.get(links.byDate, {params: {fromDate: '12-01-2020', toDate: '12-14-2020'}}).then((response) => {
-            const filtered = response.data.filter((value, index) => {
-                return index % 8 === 0;
+        const areDatesSelected = fromDate && toDate;
+
+        if ((fromDate !== null && toDate !== null) || emeters.length === 0) {
+            axios.get(areDatesSelected ? links.byDate : links.all, areDatesSelected ? {params: {fromDate: fromDate.toISOString(), toDate: toDate.toISOString()}} : null).then((response) => {
+                const filtered = response.data.filter((value, index) => {
+                    return index % (response.data.length > 10000 ? 8 : 4) === 0;
+                });
+                setEmeters(filtered);
+                setIsEmeterLoading(false);
+            }).catch((error) => {
+                console.error(error);
+                setIsEmeterLoading(false);
             });
-            setEmeters(filtered);
-            setIsEmeterLoading(false);
-        }).catch((error) => {
-            console.error(error);
-            setIsEmeterLoading(false);
-        });
-    }, []);
+        }
+    }, [fromDate, toDate]);
 
     return <>
-        {isEmeterLoading ? <CircularProgress color="secondary"/> :
+        <div className={'datePickers'}>
+            From <DatePicker selected={fromDate} onChange={date => setFromDate(date)}/><br/>
+            To <DatePicker selected={toDate} onChange={date => setToDate(date)}/>
+        </div>
+        {isEmeterLoading ? <CircularProgress/> :
             emeters ? <GeneralLineChart chartData={emeters}/> : null}
-    </>
+    </>;
 }
 
 export default GeneralLineChartLayout;
